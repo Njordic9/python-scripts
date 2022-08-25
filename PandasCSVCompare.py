5897a2cff3a45db3e25f4f963eb5f775be4c4160
Njordic9 <26882472+Njordic9@users.noreply.github.com>
2022-08-25 14:26:07 -0500

    Update PandasCSVCompare.py

--- PandasCSVCompare.py
+++ PandasCSVCompare.py
@@ -13,18 +13,10 @@ with open('data_diff.csv', 'w') as outFile:
             outFile.write(row)
 
 
-
-
-
 # csvoutfile = open("filelocation.csv", 'wt', encoding='utf8',newline='')
 # outFile = csv.writer(csvoutfile, delimiter=",")
-
-
-
 # f = open("outputfile.csv", 'rt')
-
 # reader = csv.reader(f,delimiter=',')
-
 # for row in reader:
 #                 if 'year' in row[1]:
 #                                 print (row[0])
